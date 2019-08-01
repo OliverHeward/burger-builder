4abce2c41e610cece3b2c9f9074cbf4ef93a492b
Oliver Heward <oliverhewardbie@gmail.com>
2019-08-01 18:04:43 +0100

    Been working through making everything run through Redux, including orders saved within firebase. Huge restructing went on

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order';
 
 const logger = store => {
     return next => {
@@ -20,10 +22,14 @@ const logger = store => {
     }
 };
 
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer
+});
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer,composeEnhancers(applyMiddleware(logger)));
-
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(logger, thunk)));
 
 // Setting React Redux & Router Dom
 const app = (
