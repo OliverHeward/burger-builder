0837a5ae22ba2492bfe35d444cd96ad59f8943e0
Oliver Heward <oliverhewardbie@gmail.com>
2019-08-02 17:01:33 +0100

    reduced the reducers to have more readability, improved performance of other components. Begun to add Auth through Firebase

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,6 +8,8 @@ const navigationItems = () => (
             link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem 
             link="/orders">Orders</NavigationItem>
+        <NavigationItem
+            link="/auth">Authenticate</NavigationItem>
     </ul>
 );
 
--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     ingredients: null,
@@ -13,54 +14,52 @@ const INGREDIENT_PRICES = {
     bacon: 0.7
 }
 
+const addIngredient = (state, action) => {
+    const updatedIngredient = { [action.ingredientName] : state.ingredients[action.ingredientName] + 1 }
+    const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+    const updatedState = {
+        ingredients: updatedIngredients,
+        // takes old price and adds the ingredient price for the given payload name
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+    return updateObject(state, updatedState);
+};
+
+const removeIngredient = (state, action) => {
+    const updatedIng = { [action.ingredientName] : state.ingredients[action.ingredientName] + 1 }
+    const updatedIngs = updateObject(state.ingredients, updatedIng);
+    const updatedSt = {
+        ingredients: updatedIngs,
+        // takes old price and adds the ingredient price for the given payload name
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+    return updateObject(state, updatedSt);
+};
+
+const setIngredients = (state, action) => {
+    return updateObject(state, {
+        ingredients: {
+            salad: action.ingredients.salad,
+            bacon: action.ingredients.bacon,
+            cheese: action.ingredients.cheese,
+            meat: action.ingredients.meat
+        },
+        totalPrice: 4,
+        error: false
+    });
+};
+
+const fetchIngredientsFailed = (state, action) => {
+    return updateObject(state, {error : true })
+};
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                // immutably creating new ingredients object
-                ingredients: {
-                    // cloning the prev ingredients
-                    ...state.ingredients,
-                    // expected ingredient from action payload - then update state of ingredient payload + 1
-                    [action.ingredientName] : state.ingredients[action.ingredientName] + 1
-                },
-                // takes old price and adds the ingredient price for the given payload name
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            };
-        case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                // immutably creating new ingredients object
-                ingredients: {
-                    // cloning the prev ingredients
-                    ...state.ingredients,
-                     // expected ingredient from action payload - then update state of ingredient payload minus 1
-                    [action.ingredientName] : state.ingredients[action.ingredientName] - 1
-                },
-                 // takes old price and minus' the ingredient price for the given payload name
-                 totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-            };
-            case actionTypes.SET_INGREDIENTS:
-                return {
-                    ...state,
-                    ingredients: {
-                        salad: action.ingredients.salad,
-                        bacon: action.ingredients.bacon,
-                        cheese: action.ingredients.cheese,
-                        meat: action.ingredients.meat
-                    },
-                    totalPrice: 4,
-                    error: false
-                };
-            case actionTypes.FETCH_INGREDIENTS_FAILED: 
-                return {
-                    ...state,
-                    error: true
-                }
-            default:
-                return state;
+        case actionTypes.ADD_INGREDIENT : return addIngredient(state, action);
+        case actionTypes.REMOVE_INGREDIENT : return removeIngredient(state, action);
+        case actionTypes.SET_INGREDIENTS : return setIngredients(state, action);
+        case actionTypes.FETCH_INGREDIENTS_FAILED : return fetchIngredientsFailed(state, action);
+        default : return state;
     }
 };
 
